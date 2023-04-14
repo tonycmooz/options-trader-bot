eb7340dae1caa2765c5b099cee85f76429f31942
Anthony Mooz <tonycmooz@gmail.com>
2023-04-14 13:36:44 +0200

    adding more strategies

--- auto_trader.py
+++ auto_trader.py
@@ -2,12 +2,16 @@ import pandas as pd
 import numpy as np
 import requests
 import logging
-import robin_stocks as r  # If using Robinhood
+import robin_stocks as r  # Unofficial API, if using Robinhood
 
 logging.basicConfig(level=logging.INFO)
 
-# Authenticate with your broker
-# Example API Authentication with the popular Robinhood App
+"""
+Authenticate with your broker
+Example API Authentication with the popular Robinhood App
+This Robinhood API is not officially supported. 
+USE CAUTION WHEN PLACING REAL TRADES.
+"""
 
 username = 'your_username'
 password = 'your_password'
@@ -145,3 +149,84 @@ def bear_put_spread(symbol, expiration_date):
     lower_option_id = get_option_id(put_options, lower_strike_price)
     sell_order = place_order(lower_option_id, 'sell', 1)
     logging.info(f"Sell put option with a lower strike price ({lower_strike_price}) on {symbol} - Order ID: {sell_order['id']}")
+
+def select_atm_strike_price(options):
+    """
+    This function selects the At-the-Money (ATM) strike price based on the 
+    current price of the underlying asset. 
+
+    This function takes a list of option contracts as input and calculates 
+    the strike price that is closest to the current price of the underlying asset. 
+    It returns the strike price that has the smallest absolute difference with 
+    the underlying asset price. This strike price is considered the At-the-Money 
+    strike price.
+
+    (You might want to modify this function based on your specific requirements,
+    such as choosing the nearest Out-of-the-Money or In-the-Money strike price.
+    """
+    underlying_price = float(r.stocks.get_latest_price(options[0]['chain_symbol'])[0])
+
+    closest_strike_price = float(options[0]['strike_price'])
+    min_difference = abs(underlying_price - closest_strike_price)
+
+    for option in options[1:]:
+        strike_price = float(option['strike_price'])
+        difference = abs(underlying_price - strike_price)
+
+        if difference < min_difference:
+            min_difference = difference
+            closest_strike_price = strike_price
+
+    return closest_strike_price
+
+def bullish_calendar_spread_calls(symbol, near_expiration_date, far_expiration_date):
+    """
+    A Calendar Spread strategy involves buying and selling options with 
+    the same strike price but different expiration dates. This strategy 
+    is typically used when an investor expects the underlying asset's price 
+    to remain stable in the short term but move in the long term.
+    """
+    # Get call options for both expiration dates
+    near_option_chain = get_option_chain(symbol, near_expiration_date)
+    far_option_chain = get_option_chain(symbol, far_expiration_date)
+
+    near_call_options = [option for option in near_option_chain if option['type'] == 'call']
+    far_call_options = [option for option in far_option_chain if option['type'] == 'call']
+
+    # Select the At-the-Money strike price
+    strike_price = select_atm_strike_price(near_call_options)
+
+    # Sell a near-term call option
+    near_option_id = get_option_id(near_call_options, strike_price)
+    sell_near_order = place_order(near_option_id, 'sell', 1)
+    logging.info(f"Sell near-term call option with strike price ({strike_price}) on {symbol} - Order ID: {sell_near_order['id']}")
+
+    # Buy a longer-term call option
+    far_option_id = get_option_id(far_call_options, strike_price)
+    buy_far_order = place_order(far_option_id, 'buy', 1)
+    logging.info(f"Buy longer-term call option with strike price ({strike_price}) on {symbol} - Order ID: {buy_far_order['id']}")
+
+def long_straddle(symbol, expiration_date):
+    """
+    A Straddle strategy involves buying a call option and a put option with 
+    the same strike price and expiration date. This strategy is used when 
+    an investor expects a significant price movement in the underlying asset 
+    but is uncertain about the direction.
+    """
+    # Get call and put options
+    option_chain = get_option_chain(symbol, expiration_date)
+    call_options = [option for option in option_chain if option['type'] == 'call']
+    put_options = [option for option in option_chain if option['type'] == 'put']
+
+    # Select the At-the-Money strike price
+    strike_price = select_atm_strike_price(call_options)
+
+    # Buy a call option
+    call_option_id = get_option_id(call_options, strike_price)
+    buy_call_order = place_order(call_option_id, 'buy', 1)
+    logging.info(f"Buy call option with strike price ({strike_price}) on {symbol} - Order ID: {buy_call_order['id']}")
+
+    # Buy a put option
+    put_option_id = get_option_id(put_options, strike_price)
+    buy_put_order = place_order(put_option_id, 'buy', 1)
+    logging.info(f"Buy put option with strike price ({strike_price}) on {symbol} - Order ID: {buy_put_order['id']}")
